1c559dc058898fcaca8679f5586bf743cc28c06c
Eddysdff <100467007@qq.com>
2024-11-26 08:04:15 +0800

    deploy on arlink

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,23 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   images: {
-    unoptimized: true,
+    domains: ['arweave.net'],
   },
-  trailingSlash: true,
-  assetPrefix: './',
+  env: {
+    NEXT_PUBLIC_ARWEAVE_GATEWAY: process.env.NEXT_PUBLIC_ARWEAVE_GATEWAY,
+    NEXT_PUBLIC_AO_PROCESS_ID: process.env.NEXT_PUBLIC_AO_PROCESS_ID,
+  },
   webpack: (config) => {
     config.externals.push({
       'warp-arbundles': 'warp-arbundles',
     });
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+    };
     return config;
   },
 };
